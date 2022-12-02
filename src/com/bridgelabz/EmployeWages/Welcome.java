934e569378dcdec020eddfedf17081178b37c000
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2022-12-02 02:51:14 -0800

    Welcome to Employee Wage Computation Programme

---
+++ src/com/bridgelabz/EmployeWages/Welcome.java
@@ -0,0 +1,11 @@
+
+
+package com.bridgelabz.EmployeWages;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeWage Computation Program");
+	}
+	
+
+}
